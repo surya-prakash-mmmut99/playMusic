f127955d216c5076655ea5dd2e63f2d226db6f50
Surya Prakash <suryabhai9918622@gmail.com>
2022-05-24 17:00:31 +0530

    play with drum kit

---
+++ script.js
@@ -0,0 +1,74 @@
+
+var numofButton= document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numofButton;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    
+   var buttonInner =this.innerHTML;
+   
+    makeSound(buttonInner);
+    buttonAnimation(buttonInner);
+
+    });
+    
+}
+
+ document.addEventListener("keypress",function(event){
+   makeSound(event.key);
+   buttonAnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch(key){
+
+        case "f":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        break;
+
+        case "a" :
+            audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+        break;    
+        case "s" :
+            audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+        break;    
+        case "d" :
+             audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+        break;    
+        case "j" :
+             audio = new Audio("sounds/crash.mp3");
+            audio.play();
+        break;    
+        case "k" :
+            audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        break;    
+        case "l" :
+             audio = new Audio("sounds/snare.mp3");
+            audio.play();
+        break;  
+
+        
+    }
+
+}
+
+function buttonAnimation(currentKey){
+ var activeButton =   document.querySelector("."+currentKey);
+
+ activeButton.classList.add("pressed");
+
+ setTimeout(function(){
+    activeButton.classList.remove("pressed");
+ },100)
+
+}
+
+
+ 
+
